1377457b9ad8bd3a28ab3b140d919fbd587e0bd7
alexdavies440 <alexdavies440@gmail.com>
2024-09-29 16:49:18 -0500

    simple controllers

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,13 +2,27 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping
+    @GetMapping("hello")
     @ResponseBody
     public String hello() {
         return "Hello, Spring";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring";
+    }
+
+    @RequestMapping(value = "hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hellogoodbye() {
+        return "Hello Spring. Goodbye Spring.";
     }
 }
