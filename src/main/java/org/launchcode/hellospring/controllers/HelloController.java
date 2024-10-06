6b3a6e7dbce363401bac78eda66788ffc28ff31f
alexdavies440 <alexdavies440@gmail.com>
2024-10-06 18:29:37 -0500

    First template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,31 +8,29 @@ import javax.imageio.plugins.tiff.GeoTIFFTagSet;
 @Controller
 public class HelloController {
     @GetMapping()
-    @ResponseBody
     public String hello() {
         return "root";
     }
 
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring";
     }
 
     @RequestMapping(value = "hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
+
     public String hellogoodbye() {
         return "Hello Spring. Goodbye Spring.";
     }
 
    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-   @ResponseBody
+
    public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
    }
 
     @GetMapping("hello/{name}")
-    @ResponseBody
+
 
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
@@ -45,16 +43,9 @@ public class HelloController {
     }
 
     @GetMapping("form")
-    @ResponseBody
+
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +
-                "<input type='text' name ='name'>" + //submit request to /hello
-                "<input type='submit' value='greet me'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
 }
