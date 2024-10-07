4ff14af4c4b7a2d1e5ae70bd87a8cdd119164f17
alexdavies440 <alexdavies440@gmail.com>
2024-10-06 20:11:11 -0500

    hello-list

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,16 +1,16 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.plugins.tiff.GeoTIFFTagSet;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
-    @GetMapping()
-    public String hello() {
-        return "root";
-    }
+//
 
     @GetMapping("goodbye")
     public String goodbye() {
@@ -25,15 +25,18 @@ public class HelloController {
 
    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
 
-   public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+   public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
    }
 
     @GetMapping("hello/{name}")
 
-
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // If no name given, redirects to root
@@ -46,6 +49,18 @@ public class HelloController {
 
     public String helloForm() {
         return "form";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Alex");
+        names.add("Aldo");
+        names.add("Lucy");
+        names.add("Looney");
+
+        model.addAttribute("names", names);
+
+        return "hello-list";
     }
 
 }
